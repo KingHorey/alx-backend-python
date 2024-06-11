062f97402bf7a0a77573d83a5a5ed31e56cfd996
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-11 15:41:45 +0100

    correct annotations to param

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 """ import the async module """
 
 
-async def wait_random(int: max_delay = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ async coroutine """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
