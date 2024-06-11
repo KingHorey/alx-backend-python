9dbe6987b942dc5e1e83db7bb92e4f25a55c591b
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-11 15:58:09 +0100

    add return annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list:
     """ run wait_random n times """
     output = []
     for i in range(n):
